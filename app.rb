be816528845b961b9a4ab8c9e9910e2584b13c72
Natalie Emanuel <natalieazana@gmail.com>
2018-07-17 16:52:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,21 +1,15 @@
 require_relative 'config/environment'
 
-# class App < Sinatra::Base
-#   get '/name' do
-#     "My name is Natalie"
-#   end 
+class App < Sinatra::Base
+  get '/name' do
+  "My name is Natalie"
+  end 
 
-#   get '/hometown' do 
-#     "My hometown is Dallas"
-#   end 
+get '/hometown' do 
+  "My hometown is Dallas"
+end 
  
-#   get '/favorite-song' do
-#     "My favorite song is Ghosts n Stuff"
-#   end 
-# end  
-
-class App
-  get '/' do
-    "Hello World"
-  end
-end
+get '/favorite-song' do
+  "My favorite song is Ghosts 'n' Stuff"
+end 
+end  
